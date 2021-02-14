54592dd9f803f7086f2fe590197906f7f062ae4e
coco-bass <coco.bass.0826@gmail.com>
2021-02-14 15:57:49 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
 
 import { getEvent, deleteEvent, putEvent } from "../actions";
 
@@ -25,10 +27,14 @@ class EventsShow extends Component {
       meta: { touched, error },
     } = field;
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     );
   }
 
@@ -45,6 +51,7 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 };
     return (
       <React.Fragment>
         <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -65,15 +72,22 @@ class EventsShow extends Component {
             />
           </div>
           <div>
-            <input
+            <RaisedButton
+              label="Submit"
               type="submit"
-              value="Submit"
+              style={style}
               disabled={pristine || submitting || invalid}
             />
-            <Link to="/">Cancel</Link>
-            <Link to="/" onClick={this.onDeleteClick}>
-              Delete
-            </Link>
+            <RaisedButton
+              label="Cancel"
+              style={style}
+              containerElement={<Link to="/" />}
+            />
+            <RaisedButton
+              label="Delete"
+              style={style}
+              onClick={this.onDeleteClick}
+            />
           </div>
         </form>
       </React.Fragment>
