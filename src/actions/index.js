2dbe51a0aa134aab15802ecf34a014c43539dc49
coco-bass <coco.bass.0826@gmail.com>
2021-02-14 10:51:42 +0900

    implement delete feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,23 @@
 import axios from "axios";
 
 export const READ_EVENTS = "READ_EVENTS";
-export const CREATE_EVENTS = "CREATE_EVENTS";
+export const CREATE_EVENT = "CREATE_EVENT";
+export const DELETE_EVENT = "DELETE_EVENT";
 
 const URL_ROOT = "https://udemy-utils.herokuapp.com/api/v1";
 const QUERYSTRING = "?token=token123";
 
 export const readEvents = () => async (dispatch) => {
   const response = await axios.get(`${URL_ROOT}/events${QUERYSTRING}`);
-  console.log(response);
   dispatch({ type: READ_EVENTS, response });
 };
 
 export const postEvent = (values) => async (dispatch) => {
   const response = await axios.post(`${URL_ROOT}/events${QUERYSTRING}`, values);
-  console.log(response);
-  dispatch({ type: CREATE_EVENTS, response });
+  dispatch({ type: CREATE_EVENT, response });
+};
+
+export const deleteEvent = (id) => async (dispatch) => {
+  await axios.delete(`${URL_ROOT}/events/${id}${QUERYSTRING}`);
+  dispatch({ type: DELETE_EVENT, id });
 };
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
-import { postEvent } from "../actions";
+import { getEvent, deleteEvent, putEvent } from "../actions";
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
   renderField(field) {
     const {
@@ -25,8 +26,14 @@ class EventsNew extends Component {
     );
   }
 
+  async onDeleteClick() {
+    const { id } = this.props.match.params;
+    await this.props.deleteEvent(id);
+    this.props.history.push("/");
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values);
+    // await this.props.postEvent(values);
     this.props.history.push("/");
   }
 
@@ -58,6 +65,9 @@ class EventsNew extends Component {
               disabled={pristine || submitting}
             />
             <Link to="/">Cancel</Link>
+            <Link to="/" onClick={this.onDeleteClick}>
+              Delete
+            </Link>
           </div>
         </form>
       </React.Fragment>
@@ -74,9 +84,9 @@ const validate = (values) => {
   return errors;
 };
 
-const mapDispatchToProps = { postEvent };
+const mapDispatchToProps = { deleteEvent };
 
 export default connect(
   null,
   mapDispatchToProps
-)(reduxForm({ validate, form: "eventNewForm" })(EventsNew));
+)(reduxForm({ validate, form: "eventShowForm" })(EventsShow));
